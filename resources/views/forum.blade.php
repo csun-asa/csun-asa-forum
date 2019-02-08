4df7e83affb32bbe5c9de294a439c591fe0425fd
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2019-02-08 11:24:00 -0800

    initial commit

---
+++ resources/views/forum.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Forum goes here</title>
+  </head>
+  <body>
+    This is where the future forum will go
+    :D
+  </body>
+</html>
